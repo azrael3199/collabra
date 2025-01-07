cd4a467665d74ec847fbfb6f108be0c7ba35a24a
Azrael <54586237+azrael3199@users.noreply.github.com>
2025-01-07 13:34:32 +0530

    Fix/UI bugs (#10)
    * fix: fix user card alignment issue
    
    * fix; added logout functionality
    
    * fix: changed to client component
    
    * Update auth-actions.ts

--- src/components/global/logout-button.tsx
+++ src/components/global/logout-button.tsx
@@ -1,12 +1,53 @@
+"use client";
 import React from "react";
 import { Button } from "../ui/button";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useToast } from "../ui/use-toast";
 
 interface LogoutButtonProps {
   children: React.ReactNode;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "btn-primary"
+    | "btn-secondary"
+    | null;
 }
 
-const LogoutButton: React.FC<LogoutButtonProps> = ({ children }) => {
-  return <Button variant="secondary">{children}</Button>;
+const LogoutButton: React.FC<LogoutButtonProps> = ({
+  children,
+  className,
+  variant,
+}) => {
+  const supabase = createClientComponentClient();
+
+  const { toast } = useToast();
+
+  const logoutUser = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      toast({
+        title: "Error",
+        variant: "destructive",
+        description: `Could not logout user: ${error}`,
+      });
+    }
+  };
+
+  return (
+    <Button
+      variant={variant ?? "secondary"}
+      className={className}
+      onClick={logoutUser}
+    >
+      {children}
+    </Button>
+  );
 };
 
 export default LogoutButton;
